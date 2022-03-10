789db5c9a8c572d67f1ce00d2ca5d564522e8e91
Gidon <gd.ryu@xsinc.co.kr>
2022-03-10 17:09:14 +0900

    rename: SideButton 컴포넌트 파일 삭제

--- src/comp-details/SideMenu.js
+++ src/comp-details/SideMenu.js
@@ -1,7 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import axios from 'axios';
-import SideButton from './SideButton';
 import '../scss/details/SideMenu.scss';
+import '../scss/details/SideButton.scss'
+
+function SideButton({ title, url }) {
+    const menuurl=`/kakaotalk/${url}`;
+    return (
+        <div>
+            <Link to={menuurl} className='SideButton'>
+                <b>
+                    {title} 
+                </b>
+            </Link>
+        </div>
+        
+    );
+}
+SideButton.defaultProps={
+    name:'',
+}
+SideButton.propTypes={
+    name: PropTypes.string,
+}
 
 function SideMenu() {
     const [sideData, setSideData]=useState({
