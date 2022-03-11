ac58db9f8ea768e48930fc6b4e2374a0d169083f
Gidon <gd.ryu@xsinc.co.kr>
2022-03-11 14:32:39 +0900

    design: 문의하기 버튼 레이아웃 수정

--- src/comp-details/RequestBtn.js
+++ src/comp-details/RequestBtn.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../scss/details/RequestBtn.scss';
 
 function RequestBtn({ onReqClick }) {
     return (
-        <div>
+        <div className='RequestBox'>
             <div className='inlineBlock'>
                 원하시는 답변을 찾지 못하셨다면, 고객센터로 문의해 주세요
             </div>
-            <button type='button' onClick={onReqClick} onKeyDown={onReqClick}>
+            <button className='inlineBlock' 
+                type='button' 
+                onClick={onReqClick} 
+                onKeyDown={onReqClick}
+            >
                     문의하기
             </button>
         </div>
