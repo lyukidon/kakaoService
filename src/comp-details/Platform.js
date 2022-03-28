b6f9e78eb7fd7fb21cfb16cb5ad9aa0a034a454f
Gidon <gd.ryu@xsinc.co.kr>
2022-03-28 11:35:54 +0900

    fix: os선택을 하지 않아도 활성화 되는 부분 수정

--- src/comp-details/Platform.js
+++ src/comp-details/Platform.js
@@ -1,33 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { setQuery } from '../modules/query';
 import '../scss/details/Platform.scss';
 
+const styled={
+    color: 'red',
+    backgroundColor: 'black',
+}
+
 function Platform({ platform, onQueryPlatform }){
     const { query }=useSelector(state=> state);
     const {service, category} = query;
+    const [toggles, setToggles]=useState({0:true})
+    const onClickToggle=(id)=>{
+        const obj=new Object;
+        obj[id] = true;
+        onQueryPlatform();
+        setToggles(obj);
+    }
 
     return (
         <div className='platformBox'>
-            {platform.map((data,index,array)=>(
-                    <div className='inlineBlock' key={data}>
-                        <span className='button'
+            {platform.map((data,index,array)=>{
+                return (
+                    <div key={data}>
+                        {console.log()}
+                        <div 
+                            className='button'
+                            className={ toggles[index] && 'active'  }
                             role='button'
                             tabIndex={index} 
-                            onClick={onQueryPlatform}
-                            onKeyDown={onQueryPlatform}
+                            onClick={()=>onClickToggle(index)}
+                            // onKeyDown={()=>onClickToggle()}
                         >   
-                            <NavLink 
+                            <NavLink
                                 to={`?service=${service}&category=${category}&platform=${index}`}
                             >
                                 {data}
                             </NavLink>
-                        </span>
-                        {index !== array.length-1 && <span>&bull;</span>}
+                        </div>
+                        {index !== array.length-1 && <div>&bull;</div>}
                     </div>
-            ))}
+                )
+            })}
         </div>
     );
 }
