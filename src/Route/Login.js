8baf66700d3fc2b9c58e114f51a05c0380a4642b
Gidon <gd.ryu@xsinc.co.kr>
2022-04-06 19:00:03 +0900

    feat: 로그인 페이지 구현 및 관리자 페이지 생성

--- src/Route/Login.js
+++ src/Route/Login.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 export default ()=>{
+	const [users, setUsers]=useState([])
+
+	useEffect(()=>{
+		axios.get('/data/users.json')
+			.then(res => setUsers([...res.data]));
+	},[])
+
+	const [input, setInput]=useState({
+		id:"",
+		pw:"",
+	})
+
+	const onChangeInput=(event)=>{
+		const { name, value }=event.target;
+		setInput({
+			...input,
+			[name]:value,
+		})
+	}
+	const onLogin=(object)=>{
+		const find=users.filter(data => data.id === object.id)[0];
+		if (!find){
+			return alert('다시 입력하세요');
+		}
+		if (find.pw === object.pw){
+			window.location.href = '/admin'
+		}else{
+			alert('다시 입력하세요');
+		}
+	}
 	return(
 		<form>
 			<div>
 				아이디
-				<input type='text'/>
+				<input type='text' name='id' onChange={onChangeInput}/>
 			</div>
 			<div>
 				비밀번호
-				<input type='password' />
+				<input type='password' name='pw' onChange={onChangeInput} />
 			</div>
-			<button type='button'>로그인</button>
+			<button type='button' onClick={()=>onLogin(input)}>로그인</button>
 		</form>
 	);
 }
