92cf4f6c6aadf8d054b3b9175f40db391c5e785f
Gidon <gd.ryu@xsinc.co.kr>
2022-04-07 13:34:45 +0900

    rename: /src/Route 이름 /src/routes로 변경 및 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/main/Footer";
-import Main from "./Route/Main";
-import Error from "./Route/Error";
-import Admin from "./Route/Admin";
+import Main from "./routes/Main";
+import Error from "./routes/Error";
+import Admin from "./routes/Admin";
 
-import Menu from "./Route/Faq";
-import Req from "./Route/Req";
-import Login from "./Route/Login";
+import Menu from "./routes/Faq";
+import Req from "./routes/Req";
+import Login from "./routes/Login";
 
 import "./scss/common.scss";
 
