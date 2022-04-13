c9972a0613ba2957c996d40ce8cab63d407ccc44
Gidon <gd.ryu@xsinc.co.kr>
2022-04-13 14:24:21 +0900

    rename: LastRequest 컴포넌트 추가

---
+++ src/components/admin/LastRequest.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LastRequest() {
+    return (
+        <div className="Box">
+            <h4>최근 문의 글</h4>
+        </div>
+    );
+}
+
+export default LastRequest;
