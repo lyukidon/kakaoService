88e8da14d772a0338c2afb818fb29100df653aa9
Gidon <gd.ryu@xsinc.co.kr>
2022-04-14 14:24:31 +0900

    fix: 로그인 상태 기본 설정 변경

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import create from "zustand";
 
 const useStore = create((set) => ({
-    login: true,
+    login: false,
     userName: "",
     toggleLogin: () => set((state) => ({ login: !state.login })),
     setUserName: (name) => set((state) => ({ userName: name })),
