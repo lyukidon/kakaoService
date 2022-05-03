0dab9fd00482b23b8d5ae4b8b7adcd71b6a444ea
Gidon <gd.ryu@xsinc.co.kr>
2022-05-03 18:54:04 +0900

    docs: /admin/dashboard "데이터 변경"에서 불필요한 코드 수정

--- src/components/admin/Article.js
+++ src/components/admin/Article.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-import Category from "./Category";
-
 const DataStat = ({ service, category, platform, article, faqData }) => (
     <div className="dataStat">
         <h4>데이터 통계</h4>
@@ -16,7 +14,7 @@ const DataStat = ({ service, category, platform, article, faqData }) => (
 );
 
 // 관리자 컴포넌트
-function EditArticle({ faqData }){
+function EditArticle({ faqData }) {
     // 선택된 옵션 값
     const [ids, setIds] = useState({
         service_id: 1,
@@ -66,57 +64,8 @@ function EditArticle({ faqData }){
             [id]: +value,
         });
     };
-    // 카테고리 추가 하기
-    const [option, setOption] = useState({
-        service: "",
-        category: "",
-        platform: "",
-    });
-    const optionValue = (event) => {
-        const { name, value } = event.target;
-        setOption({
-            ...option,
-            [name]: value,
-        });
-    };
+
     const tempStr = ["service", "category", "platform"];
-    const tempVar = [service, category, platform];
-    const tempFunc = [setService, setCategory, setPlatform];
-    const addOption = (event) => {
-        const { name } = event.target;
-        if (tempStr.indexOf(name) !== -1) {
-            const optionData = option[name];
-            if (optionData.length) {
-                tempFunc[tempStr.indexOf(name)]([
-                    ...eval(name),
-                    { content: optionData },
-                ]);
-            } else {
-                alert("내용을 입력해주세요");
-            }
-            setOption({
-                service: "",
-                category: "",
-                platform: "",
-            });
-        }
-    };
-    // 카테고리 제거하기
-    const removeOption = (event) => {
-        const { name } = event.target;
-        const index = tempStr.indexOf(name);
-        const id = ids[`${name}_id`];
-        tempFunc[index](tempVar[index].filter((c) => c[`${name}_id`] !== id));
-    };
-
-    // 글 제거하기
-    const removeArticle = (id) => {
-        setArticle(article.filter((c) => c.article_id !== id));
-    };
-
-    // 글 쓰기, 추가하기
-    const [write, setWrite] = useState(false);
-
     return (
         <>
             {/* 데이터 통계 */}
@@ -135,34 +84,30 @@ function EditArticle({ faqData }){
                 <h4>데이터 변경</h4>
                 {/* 카테고리 설정 */}
                 <div className="categorySelectAll">
-                    {tempStr.map((c) => {
-                        const variable = eval(c);
+                    {tempStr.map((dataName) => {
+                        const variable = eval(dataName);
                         return (
-                            <Category
+                            <div
+                                className="categorySelectBox"
                                 key={variable.content}
-                                data={variable}
-                                dataName={c}
-                                changeOption={changeOption}
-                                optionValue={optionValue}
-                                addOption={addOption}
-                                option={option}
-                                removeOption={removeOption}
-                            />
+                            >
+                                <div>
+                                    <select name={dataName} id="">
+                                        {variable.map((c) => (
+                                            <option
+                                                key={c.content}
+                                                id={`${dataName}_id`}
+                                                value={c[`${dataName}_id`]}
+                                                onClick={changeOption}
+                                            >
+                                                {c.content}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                            </div>
                         );
                     })}
-                    <button
-                        type="button"
-                        className="addArticleBtn"
-                        onClick={() => setWrite(!write)}
-                    >
-                        + 추가 작성
-                    </button>
-                    {write && (
-                        <div>
-                            <textarea />
-                            <button type="button">추가하기</button>
-                        </div>
-                    )}
                 </div>
 
                 <div>
@@ -171,21 +116,12 @@ function EditArticle({ faqData }){
                             <div className="articleDiv inlineBlock">
                                 {c.content}
                             </div>
-                            <div className="buttonDiv inlineBlock">
-                                <button
-                                    type="button"
-                                    onClick={() => removeArticle(c.article_id)}
-                                >
-                                    삭제
-                                </button>
-                                <button type="button">수정</button>
-                            </div>
                         </div>
                     ))}
                 </div>
             </div>
         </>
     );
-};
+}
 
 export default EditArticle;
