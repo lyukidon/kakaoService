db5367acc1edf002d191ec1b15107d42d184d14d
Gidon <gd.ryu@xsinc.co.kr>
2022-05-09 14:51:50 +0900

    design: 네비게이션 디자인 수정 및 메뉴 위치 변경

--- src/components/admin/Nav/Index.js
+++ src/components/admin/Nav/Index.js
@@ -7,23 +7,13 @@ import NavData from "./NavData";
 const TopNav = () => (
     <div className="topNav">
         <div className="leftTopNav">
-            <Link className="goMain" to="/">
-                <FontAwesomeIcon icon="fa-solid fa-headset" />
-                고객센터
-            </Link>
-            <Link className="goDash" to="/admin/dashboard">
-                <FontAwesomeIcon icon="fa-solid fa-chart-line" />
-                대시보드
-            </Link>
+            <span>
+                <FontAwesomeIcon icon="fa-solid fa-circle-user" size="lg" />{" "}
+                {window.localStorage.getItem("userId")}
+            </span>
         </div>
         <div>관리자 페이지</div>
         <div className="rightTopNav">
-            <span>
-                <em>
-                    <FontAwesomeIcon icon="fa-solid fa-circle-user" />{" "}
-                    {window.localStorage.getItem("userId")}
-                </em>
-            </span>
             <Link
                 to="/"
                 onClick={() => window.localStorage.removeItem("userId")}
@@ -39,19 +29,31 @@ function SideNav() {
     const navigate = useNavigate();
     return (
         <div className="sideNav">
-            <div>바로가기</div>
-            <ul>
+            <div>
                 {NavData.map((content, index) => (
-                    <li
+                    <div
                         role="none"
                         tabIndex={index}
                         key={content.id}
                         onClick={() => navigate(content.url)}
                     >
-                        {content.title}
-                    </li>
+                        <FontAwesomeIcon icon={content.icon} /> {content.title}
+                    </div>
                 ))}
-            </ul>
+            </div>
+            <div>
+                <div>
+                    <Link className="goMain" to="/">
+                        <FontAwesomeIcon icon="fa-solid fa-house-chimney" />{" "}
+                        고객센터
+                    </Link>
+                </div>
+                <div>
+                    <Link className="goDash" to="/admin/dashboard">
+                        <FontAwesomeIcon icon="fa-solid fa-table" /> 대쉬보드
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/admin/Nav/NavData.js
+++ src/components/admin/Nav/NavData.js
@@ -3,20 +3,24 @@ export default [
         id: 1,
         title: "트래픽 통계",
         url: "/admin/statics",
+        icon: "fa-solid fa-chart-line",
     },
     {
         id: 2,
         title: "최근 문의",
         url: "/admin/lastRequest",
+        icon: "fa-solid fa-q",
     },
     {
         id: 3,
         title: "도움말 통계",
         url: "/admin",
+        icon: "fa-solid fa-chart-bar",
     },
     {
         id: 4,
         title: "도움말 목록",
         url: "/admin/faq",
+        icon: "fa-solid fa-list",
     },
-]
+];
