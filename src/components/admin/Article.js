893be0896f14a901223b627ec0010987a4406160
Gidon <gd.ryu@xsinc.co.kr>
2022-05-11 19:03:49 +0900

    remove: Article.js 에 카테고리 설정 및 글 확인 추가, 필요없는 파일 제거

--- src/components/admin/Article.js
+++ src/components/admin/Article.js
@@ -1,35 +1,11 @@
+/* eslint react/self-closing-comp: 0 */
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const DataStat = ({ service, category, platform, article, faqData }) => (
-    <div className="dataStat">
-        <div className="titleNlink">
-            <h4>도움말 통계</h4>
-            <Link to="/admin/datastat">
-                <h4>
-                    더보기
-                    <FontAwesomeIcon
-                        className="rightArrow"
-                        icon="fa-solid fa-angle-right"
-                        size="lg"
-                    />
-                </h4>
-            </Link>
-        </div>
-        <ul>
-            <li>서비스 갯수: {service.length}</li>
-            <li>카테고리 갯수(상위 옵션 기준): {category.length}</li>
-            <li>OS 갯수(상위 옵션 기준): {platform.length}</li>
-            <li>글 갯수: {article.length}</li>
-            <li>전체 글 갯수: {faqData.article.length}</li>
-        </ul>
-    </div>
-);
+import "../../scss/admin/article.scss";
 
 // 관리자 컴포넌트
-function EditArticle({ faqData }) {
-    // 선택된 옵션 값
+function Index({ faqData }) {
     const [ids, setIds] = useState({
         service_id: 0,
         category_id: 0,
@@ -43,6 +19,9 @@ function EditArticle({ faqData }) {
     const [platform, setPlatform] = useState([]);
     const [article, setArticle] = useState([]);
 
+    useEffect(() => {
+        console.log(article);
+    }, [article]);
     // 데이터 분류하기
     useEffect(() => {
         if (faqData) {
@@ -70,7 +49,6 @@ function EditArticle({ faqData }) {
         }
     }, [faqData, ids]);
 
-    // 카테고리 클릭 시 실행되는 함수
     const changeOption = (event) => {
         const { id, value } = event.target;
         setIds({
@@ -78,84 +56,59 @@ function EditArticle({ faqData }) {
             [id]: +value,
         });
     };
-    const tempStr = ["service", "category", "platform"];
 
     return (
-        <>
-            {/* 데이터 통계 */}
-            {faqData && (
-                <DataStat
-                    service={service}
-                    category={category}
-                    platform={platform}
-                    article={article}
-                    faqData={faqData}
-                />
-            )}
-
-            {/* 데이터 변경 */}
-            <div className="article">
-                <div className="titleNlink">
-                    <h4>도움말 목록</h4>
-                    <Link to="/admin/faq">
-                        <h4>
-                            더보기
-                            <FontAwesomeIcon
-                                className="rightArrow"
-                                icon="fa-solid fa-angle-right"
-                                size="lg"
-                            />
-                        </h4>
-                    </Link>
-                </div>
-                {/* 카테고리 설정 */}
-                <div className="categorySelectAll">
-                    {tempStr.map((dataName, index, arr) => {
-                        const variable = eval(dataName);
-                        return (
-                            <div
-                                className="categorySelectBox"
-                                key={variable.content}
-                            >
-                                <div>
-                                    <select name={dataName} id="">
-                                        {variable.map((c) => (
-                                            <option
-                                                key={c.content}
-                                                id={`${dataName}_id`}
-                                                value={c[`${dataName}_id`]}
-                                                onClick={changeOption}
-                                            >
-                                                {c.content}
-                                            </option>
-                                        ))}
-                                    </select>
-                                </div>
-                                {index !== arr.length - 1 && (
-                                    <FontAwesomeIcon
-                                        className="rightArrow"
-                                        icon="fa-solid fa-angle-right"
-                                        size="xl"
-                                    />
-                                )}
-                            </div>
-                        );
-                    })}
-                </div>
-
-                <div>
-                    {article.map((c,i) => (
-                        i < 8 && ( 
-                        <div key={c.article_id} className="articleBox">
-                            <div className="articleDiv inlineBlock">
-                                {c.content}
-                            </div>
-                        </div>)
+        <div className="editArticle">
+            <div className="categorySelectAll">
+                <select name="service" id="">
+                    <option value="">선택해주세요</option>
+                    {service.map((c) => (
+                        <option
+                            key={c.content}
+                            id="service_id"
+                            value={c.service_id}
+                            onClick={(event) => changeOption(event)}
+                        >
+                            {c.content}
+                        </option>
                     ))}
-                </div>
+                </select>
+                <FontAwesomeIcon icon="fa-solid fa-angle-right" size="xl" />
+                <select name="" id="">
+                    <option value="">선택해주세요</option>
+                    {category.map((c) => (
+                        <option
+                            key={c.content}
+                            id="category_id"
+                            value={c.category_id}
+                            onClick={(event) => changeOption(event)}
+                        >
+                            {c.content}
+                        </option>
+                    ))}
+                </select>
+                <FontAwesomeIcon icon="fa-solid fa-angle-right" size="xl"/>
+                <select name="" id="">
+                    <option value="">선택해주세요</option>
+                    {platform.map((c) => (
+                        <option
+                            key={c.content}
+                            id="platform_id"
+                            value={c.platform_id}
+                            onClick={(event) => changeOption(event)}
+                        >
+                            {c.content}
+                        </option>
+                    ))}
+                </select>
             </div>
-        </>
+            <div>
+                {article.map((c) => (
+                    <div key={c.article_id}>{c.content}</div>
+                ))}
+            </div>
+        </div>
     );
 }
 
-export default EditArticle;
+export default Index;
