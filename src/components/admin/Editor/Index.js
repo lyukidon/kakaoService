8c4727bb00405b75eadbf2f6e1e06b1fdd6023ae
Gidon <gd.ryu@xsinc.co.kr>
2022-05-13 12:30:05 +0900

    feat: 에디터 라이브러리 추가 (TinyMCE)

--- src/components/admin/Editor/Index.js
+++ src/components/admin/Editor/Index.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Editor(props) {
+function Index() {
+    const editorRef = useRef(null);
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent());
+        }
+    };
     return (
-        <div className='editor'>
+        <div className="editor">
+            <div>도움말을 선택해주세요</div>
             <div>
-                도움말을 선택해주세요
+                <Editor
+                    apiKey="hfwmexaein3epohgzx9107h3evusuan35khip2qzgwwo0n1m"
+                    onInit={(evt, editor) => (editorRef.current = editor)}
+                    initialValue="<p>This is the initial content of the editor.</p>"
+                    init={{
+                        height: 500,
+                        menubar: false,
+                        plugins: [
+                            "advlist",
+                            "autolink",
+                            "lists",
+                            "link",
+                            "image",
+                            "charmap",
+                            "anchor",
+                            "searchreplace",
+                            "visualblocks",
+                            "code",
+                            "fullscreen",
+                            "insertdatetime",
+                            "media",
+                            "table",
+                            "preview",
+                            "help",
+                            "wordcount",
+                        ],
+                        toolbar:
+                            "undo redo | blocks | " +
+                            "bold italic forecolor | alignleft aligncenter " +
+                            "alignright alignjustify | bullist numlist outdent indent | " +
+                            "removeformat | help",
+                        content_style:
+                            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                    }}
+                />
+                <button type="button" onClick={log}>
+                    <FontAwesomeIcon icon="fa-solid fa-floppy-disk" size="lg" />
+                    저장하기
+                </button>
             </div>
         </div>
     );
 }
 
-export default Editor;
+export default Index;
