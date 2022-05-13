882fade3148bd55dc7684fb70e5020973def870b
Gidon <gd.ryu@xsinc.co.kr>
2022-05-13 11:30:44 +0900

    design: /admin/faq '도움말을 선택해주세요' 표시 중앙 배치'

--- src/components/admin/Editor/Index.js
+++ src/components/admin/Editor/Index.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Editor(props) {
     return (
         <div className='editor'>
-            글을 선택해주세요
+            <div>
+                도움말을 선택해주세요
+            </div>
         </div>
     );
 }
