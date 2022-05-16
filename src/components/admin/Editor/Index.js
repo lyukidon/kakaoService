87d87fa16f80d427f22cce40495506c995661102
Gidon <gd.ryu@xsinc.co.kr>
2022-05-16 12:23:17 +0900

    feat: /admin/faq 필드명 추가

--- src/components/admin/Editor/Index.js
+++ src/components/admin/Editor/Index.js
@@ -3,67 +3,82 @@ import { Editor } from "@tinymce/tinymce-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Index({ articleId, setArticleId, singleArti }) {
-    const  titleRef = useRef(null)
+    const titleRef = useRef(null);
     const editorRef = useRef(null);
     const log = () => {
         if (editorRef.current && titleRef.current) {
-            console.log(titleRef.current.value)
+            console.log(titleRef.current.value);
             console.log(editorRef.current.getContent());
         }
     };
     return (
         <div className={articleId !== -1 ? "editor" : "editor center"}>
             {articleId === -1 && (
-                <div className="request"><strong>도움말을 선택해주세요</strong></div>
+                <div className="request">
+                    <strong>도움말을 선택해주세요</strong>
+                </div>
             )}
             {articleId !== -1 && (
                 <>
-                    <input type="text" placeholder="제목" ref={titleRef} value={singleArti.content} />
-                    <Editor
-                        apiKey="hfwmexaein3epohgzx9107h3evusuan35khip2qzgwwo0n1m"
-                        onInit={(evt, editor) => (editorRef.current = editor)}
-                        initialValue={singleArti.explain}
-                        init={{
-                            selector:'textarea',
-                            placeholder: '내용',
-                            resize: "both",
-                            plugins: [
-                                "advlist",
-                                "autolink",
-                                "autoresize",
-                                "lists",
-                                "link",
-                                "image",
-                                "charmap",
-                                "anchor",
-                                "searchreplace",
-                                "visualblocks",
-                                "code",
-                                "fullscreen",
-                                "insertdatetime",
-                                "media",
-                                "table",
-                                "preview",
-                                "help",
-                                "wordcount",
-                            ],
-                            min_height: 500,
-                            toolbar:
-                                "undo redo | blocks | " +
-                                "bold italic forecolor | alignleft aligncenter " +
-                                "alignright alignjustify | bullist numlist outdent indent | " +
-                                "removeformat | help",
-                            content_style:
-                                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px;}",
-                        }}
-                    />
-                    <button type="button" onClick={log}>
-                        <FontAwesomeIcon
-                            icon="fa-solid fa-floppy-disk"
-                            size="lg"
+                    <div className="content">
+                        <div>제목</div>
+                        <input
+                            type="text"
+                            placeholder="제목을 입력해주세요"
+                            ref={titleRef}
+                            value={singleArti.content}
                         />
-                        저장하기
-                    </button>
+                    </div>
+                    <div className="explain">
+                        <div>내용</div>
+                        <Editor
+                            apiKey="hfwmexaein3epohgzx9107h3evusuan35khip2qzgwwo0n1m"
+                            onInit={(evt, editor) =>
+                                (editorRef.current = editor)
+                            }
+                            initialValue={singleArti.explain}
+                            init={{
+                                selector: "textarea",
+                                placeholder: "내용",
+                                resize: "both",
+                                plugins: [
+                                    "advlist",
+                                    "autolink",
+                                    "autoresize",
+                                    "lists",
+                                    "link",
+                                    "image",
+                                    "charmap",
+                                    "anchor",
+                                    "searchreplace",
+                                    "visualblocks",
+                                    "code",
+                                    "fullscreen",
+                                    "insertdatetime",
+                                    "media",
+                                    "table",
+                                    "preview",
+                                    "help",
+                                    "wordcount",
+                                ],
+                                min_height: 500,
+                                toolbar:
+                                    "undo redo | blocks | " +
+                                    "bold italic forecolor | alignleft aligncenter " +
+                                    "alignright alignjustify | bullist numlist outdent indent | " +
+                                    "removeformat | help",
+                                content_style:
+                                    "body { font-family:Helvetica,Arial,sans-serif; font-size:14px;}",
+                            }}
+                        />
+                        <button type="button" onClick={log}>
+                            <FontAwesomeIcon
+                                icon="fa-solid fa-floppy-disk"
+                                size="lg"
+                            />
+                            저장하기
+                        </button>
+                    </div>
                 </>
             )}
         </div>
