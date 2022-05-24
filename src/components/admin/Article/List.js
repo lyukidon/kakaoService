197f2bb5fa99c66c69ee4f33c395b3d83d26d923
Gidon <gd.ryu@xsinc.co.kr>
2022-05-24 10:21:50 +0900

    docs: /admin 도움말 렌더링 갯수 설정 초기화 ( 전체 렌더링 )

--- src/components/admin/Article/List.js
+++ src/components/admin/Article/List.js
@@ -56,92 +56,87 @@ export default function List({
 }) {
     return (
         <>
-            {article.map(
-                (c, i) =>
-                    i < 8 && (
-                        <div
-                            key={c.article_id}
-                            className={
-                                c.article_id === articleId
-                                    ? "articleBox if-article-chosen"
-                                    : "articleBox"
-                            }
-                        >
-                            <div className="badge">
-                                {service[c.service_id - 1].content}
-                            </div>
-                            <div
-                                role="button"
-                                tabIndex={0}
+            {article.map((c) => (
+                <div
+                    key={c.article_id}
+                    className={
+                        c.article_id === articleId
+                            ? "articleBox if-article-chosen"
+                            : "articleBox"
+                    }
+                >
+                    <div className="badge">
+                        {service[c.service_id - 1].content}
+                    </div>
+                    <div
+                        role="button"
+                        tabIndex={0}
+                        className={
+                            editor ? "content if-editor-true" : "content"
+                        }
+                        onClick={() => {
+                            setActivateEditor(false);
+                            handleFunc(c, preview, setPreview);
+                        }}
+                        // onKeyDown={() => setPreview(!preview)}
+                    >
+                        {c.content}
+                    </div>
+                    {editor && (
+                        <div className="articlebutton">
+                            {/* 수정버튼 */}
+                            <button
+                                type="button"
                                 className={
-                                    editor
-                                        ? "content if-editor-true"
-                                        : "content"
+                                    activateEditor && c.article_id === articleId
+                                        ? "edit activateEditor"
+                                        : "edit"
                                 }
                                 onClick={() => {
-                                    setActivateEditor(false)
-                                    handleFunc(c, preview, setPreview);
+                                    setChkTrash(false);
+                                    setPreview(false);
+                                    handleFunc(
+                                        c,
+                                        activateEditor,
+                                        setActivateEditor
+                                    );
                                 }}
-                                // onKeyDown={() => setPreview(!preview)}
                             >
-                                {c.content}
-                            </div>
-                            {editor && (
-                                <div className="articlebutton">
-                                    {/* 수정버튼 */}
-                                    <button
-                                        type="button"
-                                        className={activateEditor && c.article_id === articleId ? "edit activateEditor" : "edit"}
-                                        onClick={() => {
-                                            setChkTrash(false);
-                                            setPreview(false)
-                                            handleFunc(
-                                                c,
-                                                activateEditor,
-                                                setActivateEditor
-                                            );
-                                        }}
-                                    >
-                                        <FontAwesomeIcon
-                                            icon="fa-solid fa-pen-to-square"
-                                            size="lg"
-                                        />
-                                    </button>
-                                    {/* 삭제버튼 */}
-                                    <button
-                                        type="button"
-                                        className="trash"
-                                        onClick={() => {
-                                            setActivateEditor(false);
-                                            setChkTrash(!chkTrash);
-                                            handleFunc(
-                                                c,
-                                                chkTrash,
-                                                setChkTrash
-                                            );
-                                        }}
-                                    >
-                                        <FontAwesomeIcon
-                                            icon="fa-solid fa-trash"
-                                            size="lg"
-                                        />
-                                    </button>
-                                    {c.article_id === articleId && chkTrash && (
-                                        <Warning
-                                            data={c}
-                                            chkTrash={chkTrash}
-                                            setChkTrash={setChkTrash}
-                                            article={article}
-                                            setArticle={setArticle}
-                                            articleId={articleId}
-                                            handleFunc={handleFunc}
-                                        />
-                                    )}
-                                </div>
+                                <FontAwesomeIcon
+                                    icon="fa-solid fa-pen-to-square"
+                                    size="lg"
+                                />
+                            </button>
+                            {/* 삭제버튼 */}
+                            <button
+                                type="button"
+                                className="trash"
+                                onClick={() => {
+                                    setActivateEditor(false);
+                                    setChkTrash(!chkTrash);
+                                    handleFunc(c, chkTrash, setChkTrash);
+                                }}
+                            >
+                                <FontAwesomeIcon
+                                    icon="fa-solid fa-trash"
+                                    size="lg"
+                                />
+                            </button>
+                            {c.article_id === articleId && chkTrash && (
+                                <Warning
+                                    data={c}
+                                    chkTrash={chkTrash}
+                                    setChkTrash={setChkTrash}
+                                    article={article}
+                                    setArticle={setArticle}
+                                    articleId={articleId}
+                                    handleFunc={handleFunc}
+                                />
                             )}
                         </div>
-                    )
-            )}
+                    )}
+                </div>
+            ))}
         </>
     );
 }
