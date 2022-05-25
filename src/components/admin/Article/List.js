060d20c832ff98fd003d0b3a11c105d4d562de00
Gidon <gd.ryu@xsinc.co.kr>
2022-05-25 18:19:37 +0900

    feat: 페이지 버튼 기능 구현

--- src/components/admin/Article/List.js
+++ src/components/admin/Article/List.js
@@ -68,6 +68,19 @@ export default function List({
         }
         setPage([...tmparr]);
     }, [article]);
+    const [pageSelect, setPageSelect] = useState(1);
+    const [low, setLow] = useState(0);
+    const [high, setHigh] = useState(0);
+    useEffect(() => {
+        if (editor) {
+            setLow((pageSelect - 1) * 18);
+            setHigh(pageSelect * 18);
+        } else {
+            setLow((pageSelect - 1) * 8);
+            setHigh(pageSelect * 8);
+        }
+    }, [pageSelect]);
+
     return (
         <>
             {/* 헤더 */}
@@ -76,94 +89,109 @@ export default function List({
                 <div className="listTitle">제목</div>
             </div>
             {/* 도움말 목록 */}
-            {article.map((c) => (
-                <div key={c.article_id} className="articleBox">
-                    <div className="badge">
-                        <div>
-                            {category.length === 0
-                                ? service[c.service_id - 1].content
-                                : platform.length === 0
-                                ? category[c.category_id - 1].content
-                                : platform[c.platform_id - 1].content}
-                        </div>
-                    </div>
-                    <div
-                        role="button"
-                        tabIndex={0}
-                        className={`${
-                            editor ? "content if-editor-true" : "content"
-                        } ${
-                            c.article_id === articleId
-                                ? "if-article-chosen"
-                                : null
-                        }`}
-                        onClick={() => {
-                            setActivateEditor(false);
-                            handleFunc(c, preview, setPreview);
-                        }}
-                        // onKeyDown={() => setPreview(!preview)}
-                    >
-                        {c.content}
-                    </div>
-                    {editor && (
-                        <div className="articlebutton">
-                            {/* 수정버튼 */}
-                            <button
-                                type="button"
-                                className={
-                                    activateEditor && c.article_id === articleId
-                                        ? "edit activateEditor"
-                                        : "edit"
-                                }
-                                onClick={() => {
-                                    setChkTrash(false);
-                                    setPreview(false);
-                                    handleFunc(
-                                        c,
-                                        activateEditor,
-                                        setActivateEditor
-                                    );
-                                }}
-                            >
-                                <FontAwesomeIcon
-                                    icon="fa-solid fa-pen-to-square"
-                                    size="lg"
-                                />
-                            </button>
-                            {/* 삭제버튼 */}
-                            <button
-                                type="button"
-                                className="trash"
+            {article.map(
+                (c, i) =>
+                    i > low &&
+                    i < high && (
+                        <div key={c.article_id} className="articleBox">
+                            <div className="badge">
+                                <div>
+                                    {category.length === 0
+                                        ? service[c.service_id - 1].content
+                                        : platform.length === 0
+                                        ? category[c.category_id - 1].content
+                                        : platform[c.platform_id - 1].content}
+                                </div>
+                            </div>
+                            <div
+                                role="button"
+                                tabIndex={0}
+                                className={`${
+                                    editor
+                                        ? "content if-editor-true"
+                                        : "content"
+                                } ${
+                                    c.article_id === articleId
+                                        ? "if-article-chosen"
+                                        : null
+                                }`}
                                 onClick={() => {
                                     setActivateEditor(false);
-                                    handleFunc(c, chkTrash, setChkTrash);
+                                    handleFunc(c, preview, setPreview);
                                 }}
+                                // onKeyDown={() => setPreview(!preview)}
                             >
-                                <FontAwesomeIcon
-                                    icon="fa-solid fa-trash"
-                                    size="lg"
-                                />
-                            </button>
-                            {c.article_id === articleId && chkTrash && (
-                                <Warning
-                                    data={c}
-                                    chkTrash={chkTrash}
-                                    setChkTrash={setChkTrash}
-                                    article={article}
-                                    setArticle={setArticle}
-                                    articleId={articleId}
-                                    handleFunc={handleFunc}
-                                />
+                                {c.content}
+                            </div>
+                            {editor && (
+                                <div className="articlebutton">
+                                    {/* 수정버튼 */}
+                                    <button
+                                        type="button"
+                                        className={
+                                            activateEditor &&
+                                            c.article_id === articleId
+                                                ? "edit activateEditor"
+                                                : "edit"
+                                        }
+                                        onClick={() => {
+                                            setChkTrash(false);
+                                            setPreview(false);
+                                            handleFunc(
+                                                c,
+                                                activateEditor,
+                                                setActivateEditor
+                                            );
+                                        }}
+                                    >
+                                        <FontAwesomeIcon
+                                            icon="fa-solid fa-pen-to-square"
+                                            size="lg"
+                                        />
+                                    </button>
+                                    {/* 삭제버튼 */}
+                                    <button
+                                        type="button"
+                                        className="trash"
+                                        onClick={() => {
+                                            setActivateEditor(false);
+                                            handleFunc(
+                                                c,
+                                                chkTrash,
+                                                setChkTrash
+                                            );
+                                        }}
+                                    >
+                                        <FontAwesomeIcon
+                                            icon="fa-solid fa-trash"
+                                            size="lg"
+                                        />
+                                    </button>
+                                    {c.article_id === articleId && chkTrash && (
+                                        <Warning
+                                            data={c}
+                                            chkTrash={chkTrash}
+                                            setChkTrash={setChkTrash}
+                                            article={article}
+                                            setArticle={setArticle}
+                                            articleId={articleId}
+                                            handleFunc={handleFunc}
+                                        />
+                                    )}
+                                </div>
                             )}
                         </div>
-                    )}
-                </div>
-            ))}
+                    )
+            )}
             {/* Pagination */}
             <div className="page">
                 <div>
                     {page.map((c) => (
-                        <button key={c} type="button">
+                        <button
+                            key={c}
+                            type="button"
+                            onClick={() => setPageSelect(c)}
+                        >
                             {c}
                         </button>
                     ))}
