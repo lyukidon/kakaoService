8837f8440798f08dde52731b76adc2ce0c426752
Gidon <gd.ryu@xsinc.co.kr>
2022-05-26 18:52:49 +0900

    design: 미리보기 삭제 버튼 디자인 변경

--- src/components/admin/Editor/Warning.js
+++ src/components/admin/Editor/Warning.js
@@ -6,13 +6,31 @@ const Warning= styled.div`
     flex-direction: column;
     position: absolute;
     padding: 10px;
+    width: 150px;
+    right: 10px;
     background-color: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0px 0px 5px 5px #dddddd;
+    > .title{
+        margin-bottom: 10px;
+    }
+    > div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        > button {
+            padding: 0px 10px;
+            background-color: transparent;
+            border: 1px solid black;
+            border-radius: 7px;
+        }
+    }
 `;
 
 export default ({ setWarn }) => {
     return (
         <Warning>
-            <div>삭제 하시겠습니까</div>
+            <div className="title">삭제 하시겠습니까</div>
             <div>
                 <button type="button">예</button>
                 <button type="button" onClick={()=>{setWarn(false)}}>아니오</button>
