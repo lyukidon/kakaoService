27033c9119aef24ebdcb005ded57b6aded3450e4
Gidon <gd.ryu@xsinc.co.kr>
2022-06-02 18:46:12 +0900

    feat: 답변하기 클릭 시 콘솔 확인

--- src/components/admin/Qna/Editor.js
+++ src/components/admin/Qna/Editor.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Editor({ toggleId, setToggleId, toggleData, setToggleData }) {
+    const [editorData, setEditorData] = useState("");
     const titleRef = useRef(null);
     useEffect(() => {
         if (titleRef.current !== null) {
@@ -11,31 +12,40 @@ function Editor({ toggleId, setToggleId, toggleData, setToggleData }) {
         }
     }, [toggleData]);
     return (
-                <div className="editorComponent">
-                    <div className="titleBox">
-                        <div>제목:</div>
-                        <input
-                            type="text"
-                            placeholder="제목을 입력해주세요"
-                            ref={titleRef}
-                        />
-                    <div>
-                        <button type="button">
-                            <FontAwesomeIcon icon="fa-solid fa-x" />
-                            취소하기
-                        </button>
-                        <button type="button">
-                            <FontAwesomeIcon icon="fa-solid fa-paper-plane" />
-                            답변하기
-                        </button>
-                    </div>
-                    </div>
-                    <CKEditor
-                        editor={ClassicEditor}
-                        config={{ placeholder: "답변을 입력해주세요" }}
-                        data={toggleData.answer ? toggleData.answer : ""}
-                    />
+        <div className="editorComponent">
+            <div className="titleBox">
+                <div>제목:</div>
+                <input
+                    type="text"
+                    placeholder="제목을 입력해주세요"
+                    ref={titleRef}
+                />
+                <div>
+                    <button type="button">
+                        <FontAwesomeIcon icon="fa-solid fa-x" />
+                        취소하기
+                    </button>
+                    <button
+                        type="button"
+                        disabled={editorData === "" ? true : false}
+                        onClick={() => {
+                            console.log(editorData);
+                        }}
+                    >
+                        <FontAwesomeIcon icon="fa-solid fa-paper-plane" />
+                        답변하기
+                    </button>
                 </div>
+            </div>
+            <CKEditor
+                editor={ClassicEditor}
+                config={{ placeholder: "답변을 입력해주세요" }}
+                data={toggleData.answer ? toggleData.answer : ""}
+                onChange={(evt, editor) => {
+                    setEditorData(editor.getData());
+                }}
+            />
+        </div>
     );
 }
 
