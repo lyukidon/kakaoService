1432827038683464071912ff8d9c5b0392b207b4
Gidon <gd.ryu@xsinc.co.kr>
2022-06-02 15:37:59 +0900

    fix: 문의 관리 에디터 제목, 상태 관리 안되는 문제점 수정

--- src/components/admin/Qna/Editor.js
+++ src/components/admin/Qna/Editor.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 function Editor({ toggleId, setToggleId, toggleData, setToggleData }) {
+    const titleRef = useRef(null);
+    useEffect(() => {
+        titleRef.current.value = `답변: [ ${toggleData.title} ]`;
+    }, [toggleData]);
     return (
         <div className="editorComponent">
             {toggleId !== -1 && (
                 <>
                     <div className="titleBox">
                         <div>제목:</div>
-                        <input type="text" placeholder="제목을 입력해주세요" defaultValue={`답변: [ ${toggleData.title} ]`}/>
+                        <input
+                            type="text"
+                            placeholder="제목을 입력해주세요"
+                            ref={titleRef}
+                        />
                     </div>
                     <CKEditor
                         editor={ClassicEditor}
