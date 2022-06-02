b7bcb24afc5cf99718f4cf78c6cf1a7f08c233ca
Gidon <gd.ryu@xsinc.co.kr>
2022-06-02 13:51:22 +0900

    feat: /admin/qna 에디터 추가

--- src/components/admin/Qna/Editor.js
+++ src/components/admin/Qna/Editor.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-function Editor({ toggleList, setToggleList, toggleData, setToggleData }) {
+function Editor({ toggleId, setToggleId, toggleData, setToggleData }) {
     return (
-        <div className='editorComponent'>
-            에디터
+        <div className="editorComponent">
+            {toggleId !== -1 && (
+                <>
+                    <div className="titleBox">
+                        <div>제목:</div>
+                        
+                            <input type="text" />
+                        
+                    </div>
+                    <CKEditor
+                        editor={ClassicEditor}
+                        config={{ placeholder: "답변을 입력해주세요" }}
+                        data={toggleData.content}
+                    />
+                </>
+            )}
         </div>
     );
 }
