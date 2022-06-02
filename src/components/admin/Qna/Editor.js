e218bdb2214f8f3b94beafbbb22a9cf986f0bad1
Gidon <gd.ryu@xsinc.co.kr>
2022-06-02 17:56:03 +0900

    feat: 답변 달린 문의글의 경우 에디터에 데이터 표시

--- src/components/admin/Qna/Editor.js
+++ src/components/admin/Qna/Editor.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useRef } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Editor({ toggleId, setToggleId, toggleData, setToggleData }) {
     const titleRef = useRef(null);
     useEffect(() => {
-        titleRef.current.value = `답변: [ ${toggleData.title} ]`;
+        if (titleRef.current !== null) {
+            titleRef.current.value = `답변: [ ${toggleData.title} ]`;
+        }
     }, [toggleData]);
     return (
-        <div className="editorComponent">
-            {toggleId !== -1 && (
-                <>
+                <div className="editorComponent">
                     <div className="titleBox">
                         <div>제목:</div>
                         <input
@@ -22,11 +23,19 @@ function Editor({ toggleId, setToggleId, toggleData, setToggleData }) {
                     <CKEditor
                         editor={ClassicEditor}
                         config={{ placeholder: "답변을 입력해주세요" }}
-                        data=""
+                        data={toggleData.answer ? toggleData.answer : ""}
                     />
-                </>
-            )}
-        </div>
+                    <div>
+                        <button type="button">
+                            <FontAwesomeIcon icon="fa-solid fa-x" />
+                            취소하기
+                        </button>
+                        <button type="button">
+                            <FontAwesomeIcon icon="fa-solid fa-paper-plane" />
+                            답변하기
+                        </button>
+                    </div>
+                </div>
     );
 }
 
