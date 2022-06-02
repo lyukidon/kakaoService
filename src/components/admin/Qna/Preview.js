fb0807c298e511852769cc4b199d3e0084efd501
Gidon <gd.ryu@xsinc.co.kr>
2022-06-02 17:39:08 +0900

    feat: 미리보기 답변 렌더링 기능 추가

--- src/components/admin/Qna/Preview.js
+++ src/components/admin/Qna/Preview.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 
 function Preview({ toggleList, setToggleList, toggleData, setToggleData }) {
+    const previewRef = useRef(null);
+    const answerRef = useRef(null);
+    useEffect(() => {
+        previewRef.current.innerHTML = toggleData.content;
+        answerRef.current.innerHTML = toggleData.answer;
+    }, [toggleData]);
     return (
-        <div className='previewComponent'>
-            <div>
-                {toggleData.title}
-            </div>
-            <div>
-                {toggleData.content}
-            </div>
+        <div className="previewComponent">
+            <div>{toggleData.title}</div>
+            <hr />
+            <div ref={previewRef} />
+            <div>답변:</div>
+            <div ref={answerRef} />
         </div>
     );
 }
