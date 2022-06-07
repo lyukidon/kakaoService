9afad87ca5396a368744bb93d95cd8b5d1a53cba
Gidon <gd.ryu@xsinc.co.kr>
2022-06-07 12:24:52 +0900

    feat: 카테고리 변경 팝업 바탕 클릭 시, 꺼지도록 함

--- src/components/admin/Article/Category.js
+++ src/components/admin/Article/Category.js
@@ -3,7 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default ({ toggleSetting, onToggleSetting }) => {
     return (
-        <div className="settingCategory">
+        <div
+            className="settingCategory"
+            role="button"
+            tabIndex={0}
+            onClick={() => {
+                onToggleSetting(false);
+            }}
+        >
             <div className="settingModal">
                 <button
                     type="button"
