99248a4ed71e50140d68a0e066a9a12cbe779bd1
Gidon <gd.ryu@xsinc.co.kr>
2022-06-08 15:58:55 +0900

    feat: 문의 목록 페이지 기능 추가

--- src/components/admin/Qna/List.js
+++ src/components/admin/Qna/List.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function List({editor, toggleId, setToggleId, toggleData, setToggleData }) {
+function List({ editor, toggleId, setToggleId, toggleData, setToggleData }) {
     const [qnaData, setQnaData] = useState([]);
-    const [filter, setFilter] = useState([]);
+    const [filterData, setFilter] = useState([]);
     const setOption = (evt) => {
         switch (evt.target.value) {
             case "false":
@@ -25,9 +25,15 @@ function List({editor, toggleId, setToggleId, toggleData, setToggleData }) {
         }
         getData();
     }, []);
+    // pagination
+    const pageArr = new Array(Math.ceil(filterData.length / 7))
+        .fill(1)
+        .map((c, i) => c + i);
+    const [countPage, setCountPage] = useState(1);
+
     return (
         <div className="listComponent">
-            <h4 style={editor && {"margin-top":"5px"}}>문의 목록</h4>
+            <h4 style={editor && { "margin-top": "5px" }}>문의 목록</h4>
             <div>
                 <select name="" id="" onChange={(evt) => setOption(evt)}>
                     <option value="all">전체</option>
@@ -36,59 +42,82 @@ function List({editor, toggleId, setToggleId, toggleData, setToggleData }) {
                 </select>
             </div>
             <div>
-                <div className="list" style={{"border":"none"}}>
+                <div className="list" style={{ border: "none" }}>
                     <div>상태</div>
                     <div>제목</div>
                 </div>
-                {filter.map((c, i) => (
-                    <div key={c.id} className="list">
-                        <div>
-                            <span className={c.status ? "complete" : "wait"}>
-                                {c.status ? "답변완료" : "답변대기"}
-                            </span>
-                        </div>
-                        <div
-                            role="button"
-                            tabIndex={0}
-                            className={`title ${
-                                toggleId !== -1 &&
-                                c.id === toggleId &&
-                                "activate"
-                            }`}
-                            onClick={() => {
-                                if (c.id === toggleId) {
-                                    setToggleId(-1);
-                                    setToggleData({
-                                        id: 0,
-                                        title: "",
-                                        content: "",
-                                        status: false,
-                                        answer: "",
-                                    });
-                                } else {
-                                    const {
-                                        id,
-                                        title,
-                                        content,
-                                        status,
-                                        answer,
-                                    } = c;
-                                    setToggleId(c.id);
-                                    setToggleData({
-                                        ...toggleData,
-                                        id,
-                                        title,
-                                        content,
-                                        status,
-                                        answer,
-                                    });
-                                }
-                            }}
-                        >
-                            {c.title}
-                        </div>
+                {filterData.map((c, i) => {
+                    if (i < countPage * 7 && i >= (countPage - 1) * 7) {
+                        return (
+                            <div key={c.id} className="list">
+                                <div>
+                                    <span
+                                        className={
+                                            c.status ? "complete" : "wait"
+                                        }
+                                    >
+                                        {c.status ? "답변완료" : "답변대기"}
+                                    </span>
+                                </div>
+                                <div
+                                    role="button"
+                                    tabIndex={0}
+                                    className={`title ${
+                                        toggleId !== -1 &&
+                                        c.id === toggleId &&
+                                        "activate"
+                                    }`}
+                                    onClick={() => {
+                                        if (c.id === toggleId) {
+                                            setToggleId(-1);
+                                            setToggleData({
+                                                id: 0,
+                                                title: "",
+                                                content: "",
+                                                status: false,
+                                                answer: "",
+                                            });
+                                        } else {
+                                            const {
+                                                id,
+                                                title,
+                                                content,
+                                                status,
+                                                answer,
+                                            } = c;
+                                            setToggleId(c.id);
+                                            setToggleData({
+                                                ...toggleData,
+                                                id,
+                                                title,
+                                                content,
+                                                status,
+                                                answer,
+                                            });
+                                        }
+                                    }}
+                                >
+                                    {c.title}
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
+
+                <div className="page">
+                    <div>
+                        {pageArr.map((c) => (
+                            <button
+                                key={c}
+                                type="button"
+                                onClick={() => setCountPage(c)}
+                                className={`pageBtn ${countPage === c && "if-page-selected"}`}
+                            >
+                                {c}
+                            </button>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     );
