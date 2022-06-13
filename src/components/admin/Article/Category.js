afb8d004c442243bf30e36a00a26929449b1e9dd
Gidon <gd.ryu@xsinc.co.kr>
2022-06-13 12:11:51 +0900

    feat: 카테고리 설정 열고 닫기 기능 추가, 검색 기능 추가

--- src/components/admin/Article/Category.js
+++ src/components/admin/Article/Category.js
@@ -2,80 +2,104 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "@nosferatu500/react-sortable-tree/style.css";
 import FileExplorerTheme from "@nosferatu500/theme-file-explorer";
-import SortableTree, { addNodeUnderParent, getNodeAtPath, removeNodeAtPath, TextField } from "@nosferatu500/react-sortable-tree";
+import SortableTree, {
+    toggleExpandedForAll,
+    addNodeUnderParent,
+    getNodeAtPath,
+    removeNodeAtPath,
+    TextField,
+} from "@nosferatu500/react-sortable-tree";
 
-function Tree() {
-    const [state, setState] = useState({
-        treeData: [
-            {
-                title: "카카오톡",
-                children: [
-                    {
-                        title: "일반",
-                        children: [
-                            { title: "안드로이드" },
-                            { title: "iOS" },
-                            { title: "Windows" },
-                        ],
-                    },
-                    {
-                        title: "가입/변경/탈퇴",
-                        children: [
-                            { title: "안드로이드" },
-                            { title: "iOS" },
-                            { title: "안드로이드(원스토어)" },
-                        ],
-                    },
-                ],
-            },
-            {
-                title: "카카오계정",
-                children: [
-                    { title: "로그인", children: [{ title: "공통" }] },
-                    {
-                        title: "이메일/비밀번호 찾기",
-                        children: [{ title: "공통" }],
-                    },
-                ],
-            },
-            { title: "카카오 이모티콘" },
-        ],
-    });
+class Tree extends React.Component {
+    constructor(props) {
+        super(props);
 
-    const removeNode = (rowInfo) => {
-        let { node, treeIndex, path } = rowInfo;
-        removeNodeAtPath({
-            treeData: state.treeData,
-            path: path, // You can use path from here
-            getNodeKey: ({ node: TreeNode, treeIndex: number }) => {
-                console.log(number);
-                return number;
-            },
-            ignoreCollapsed: false,
-        });
-    };
-    return (
-        <div style={{ height: 500 }}>
-            <SortableTree
-                treeData={state.treeData}
-                onChange={(treeData) => setState({ treeData })}
-                onVisibilityToggle={{ expanded: true }}
-                // theme={FileExplorerTheme}
-                generateNodeProps={(rowInfo) => ({
-                    buttons: [
-                        <button
-                            onClick={(evt) => {
-                                console.log("click");
-                                removeNode(rowInfo);
-                            }}
-                        >
-                            X
-                        </button>,
+        this.state = {
+            searchString: "",
+            treeData: [
+                {
+                    title: "카카오톡",
+                    children: [
+                        {
+                            title: "일반",
+                            children: [
+                                { title: "안드로이드" },
+                                { title: "iOS" },
+                                { title: "Windows" },
+                            ],
+                        },
+                        {
+                            title: "가입/변경/탈퇴",
+                            children: [
+                                { title: "안드로이드" },
+                                { title: "iOS" },
+                                { title: "안드로이드(원스토어)" },
+                            ],
+                        },
                     ],
-                })}
-            />
-        </div>
-    );
+                },
+                {
+                    title: "카카오계정",
+                    children: [
+                        { title: "로그인", children: [{ title: "공통" }] },
+                        {
+                            title: "이메일/비밀번호 찾기",
+                            children: [{ title: "공통" }],
+                        },
+                    ],
+                },
+                { title: "카카오 이모티콘" },
+            ],
+        };
+    }
+
+    expandAndCollapse = (expanded) => {
+        this.setState(prev =>({
+            ...prev,
+            treeData: toggleExpandedForAll({
+                treeData: this.state.treeData,
+                expanded,
+            }),
+        }));
+    };
+
+    render() {
+        return (
+            <div style={{ height: 800 }}>
+                <button
+                    type="button"
+                    onClick={() => {
+                        this.expandAndCollapse(true);
+                    }}
+                >
+                    열기
+                </button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        this.expandAndCollapse(false);
+                    }}
+                >
+                    닫기
+                </button>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    onClick={(event) => {
+                        this.setState({ searchString: event.target.value });
+                    }}
+                />
+                <SortableTree
+                    treeData={this.state.treeData}
+                    searchQuery={this.state.searchString}
+                    onChange={(treeData) => this.setState({ treeData })}
+                    generateNodeProps={(rowInfo) => ({
+                        buttons: [<button onClick={() => {}}>X</button>],
+                    })}
+                />
+            </div>
+        );
+    }
 }
 
 export default ({ onToggleSetting }) => {
