e1feb00bc48250b050b6409c9650c99f9cc424df
lyukidon <64675543+lyukidon@users.noreply.github.com>
2023-04-17 10:45:28 +0900

    fix router error

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOMClient.createRoot(document.getElementById("root"))
 
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/kakaoService">
         <App />
     </BrowserRouter>
 );
